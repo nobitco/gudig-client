c022fb96d21a9215843286d637263b5572e1f57a
jesmarquez <jesmqz@gmail.com>
2017-04-21 11:13:09 -0500

    Crea getUser
    Se crea obtencion de usuario y definicion de endpoints con test.

--- test/client-test.js
+++ test/client-test.js
@@ -2,11 +2,11 @@
 
 const test = require('ava')
 const gudig = require('../')
-// const fixtures = require('./fixtures')
+const nock = require('nock')
+const fixtures = require('./fixtures')
 
 let options = {
   endpoints: {
-    pictures: 'http://gudig.test/picture',
     users: 'http://gudig.test/user',
     auth: 'http://gudig.test/auth'
   }
@@ -22,4 +22,18 @@ test('client', t => {
   t.is(typeof client.saveUser, 'function')
   t.is(typeof client.getUser, 'function')
   t.is(typeof client.auth, 'function')
+})
+
+test('getUser', async t => {
+  const client = t.context.client
+
+  let user = fixtures.getUser()
+
+  nock(options.endpoints.users)
+    .get(`/${user.username}`)
+    .reply(200, user)
+
+  let result = await client.getUser(user.username)
+
+  t.deepEqual(result, user)
 })
